947d1adeee697e6cee4846689571e7b8a92df25a
Islam <islam@Islams-MacBook-Pro-IBT.local>
2020-10-18 19:57:58 +0300

    add neovim in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+brew install neovim;
 brew install rg;
 brew install fzf;
 pip3 install --user pynvim;
