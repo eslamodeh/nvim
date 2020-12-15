9c5f4cf50f7e07122d0b0abf362c79aa6831bac7
Islam Odeh <41170621+islamodeh@users.noreply.github.com>
2020-12-15 13:43:58 +0200

    Autoload plug for nvim

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ brew install fzf;
 brew install the_silver_searcher
 pip3 install --user pynvim;
 
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+
 # To install useful key bindings and fuzzy completion:
 $(brew --prefix)/opt/fzf/install
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
