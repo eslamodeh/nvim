acfafa710b1e7ea35ef750e9e7c38bef62f6eb0f
Islam Odeh <islamodeh@Islams-Mac-mini.local>
2021-02-10 13:07:56 +0200

    cp editorconfig settings to home path

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --c
 
 # To install useful key bindings and fuzzy completion:
 $(brew --prefix)/opt/fzf/install
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
-# echo "bind-key C-Tab next-window; bind-key C-S-Tab previous-window;" > ~/.tmux.conf;
+cp .editorconfig ~/
+echo "bind-key C-Tab next-window; bind-key C-S-Tab previous-window;" > ~/.tmux.conf;
